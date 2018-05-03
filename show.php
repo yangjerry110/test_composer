e95c313836097aa7a5d8d4ebb6e2725fea5fbc57
杨杰 <yjie@rolaner.com>
2018-05-03 16:14:46 +0800

    修改

---
+++ show.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: Jerry Yang
+ * @Date:   2018-05-03 16:07:33
+ * @Last Modified by:   Jerry Yang
+ * @Last Modified time: 2018-05-03 16:09:09
+ */
+
+require "vendor/autoload.php";
+
+use Ford\test;
+
+echo Ford\test\Hello::info();
---
+++ src/Ford/test/Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @Author: Jerry Yang
+ * @Date:   2018-05-03 16:05:04
+ * @Last Modified by:   Jerry Yang
+ * @Last Modified time: 2018-05-03 16:05:55
+ */
+ namespace Ford\test;
+
+ class Hello 
+ {
+       public static function info()
+       {
+       	echo "这是一个测试";
+       }
+ }
